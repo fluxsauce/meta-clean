0f9377c5ed9148cb9887a17b9833c3412139f585
Jon Peck <jpeck@fluxsauce.com>
2021-02-08 13:45:00 -0800

    MVP

--- bin/meta-clean.js
+++ bin/meta-clean.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const chalk = require('chalk');
 const cp = require('child_process');
 const forEach = require('lodash/forEach');
 const { program } = require('commander');
@@ -9,28 +10,50 @@ const metaLib = require('../lib/meta');
 const exec = promisify(cp.exec);
 const meta = metaLib.get();
 
+async function clean(projectName) {
+  const gitPath = `${process.cwd()}/${projectName}/`;
+  const errors = [];
+  const output = [];
+
+  let branches;
+
+  try {
+    branches = await exec(`git -C ${gitPath} branch --merged | egrep -v "(^\\*|main|qa|production)"`);
+    branches = branches.stdout.trim();
+  } catch (err) {
+    output.push('Nothing to do.');
+  }
+
+  if (branches) {
+    try {
+      const result = await exec(`echo "${branches}" | xargs git -C ${gitPath} branch -d`);
+      if (result.stdout) {
+        output.push(result.stdout.trim());
+      }
+      if (result.stderr) {
+        errors.push(result.stderr.trim());
+      }
+    } catch (err) {
+      errors.push(err.message);
+    }
+  }
+
+  console.log(`${chalk.blue(projectName)}:`); // eslint-disable-line no-console
+  if (output.length > 0) {
+    output.forEach((line) => console.log(chalk.green(line))); // eslint-disable-line no-console
+    console.log(); // eslint-disable-line no-console
+  }
+
+  if (errors.length > 0) {
+    errors.forEach((line) => console.error(chalk.red(line))); // eslint-disable-line no-console
+  }
+}
+
 program
   .version('1.0.0')
   .action(() => {
     forEach(meta.projects, async (repo, projectName) => {
-      const gitPath = `${process.cwd()}/${projectName}/`;
-      try {
-        const branches = await exec(`git -C ${gitPath} branch --merged | egrep -v "(^\\*|main|qa|production)"`);
-        if (branches.stderr) {
-          console.err(branches.stderr.trim()); // eslint-disable-line no-console
-        }
-        if (branches.stdout.trim()) {
-          const result = await exec(`echo "${branches.stdout.trim()}" | xargs git -C ${gitPath} branch -d`);
-          if (result.stdout) {
-            console.log(result.stdout.trim()); // eslint-disable-line no-console
-          }
-          if (result.stderr) {
-            console.err(result.stderr.trim()); // eslint-disable-line no-console
-          }
-        }
-      } catch (err) { // eslint-disable-line no-unused-vars
-        console.error(err.message); // eslint-disable-line no-console
-      }
+      await clean(projectName);
     });
   });
 
